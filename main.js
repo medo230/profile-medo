60c6f4e5f27140777e701fd4e94f48bb7e02c9bb
ahmedknaiem‏ <a68520244@gmail.com>
2023-09-15 17:59:08 +0300

    portoflio

---
+++ main.js
@@ -0,0 +1,38 @@
+$(window).scroll(function () {
+    let x = $(window).scrollTop()
+    if (x > 200) {
+        $('.back-to-top').css('display', 'block')
+    } else {
+        $('.back-to-top').css('display', 'none')
+
+    }
+})
+
+$(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+  
+  
